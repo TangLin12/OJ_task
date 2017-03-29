b68d4a2f8aeecddc304d8506a92229a3375e9292
tang <370458781@qq.com>
2017-03-29 22:39:05 +0800

    finish SIN
    SIN()

--- LanQiaoCUP/JiShuanKe/Binary.cpp
+++ LanQiaoCUP/JiShuanKe/Binary.cpp
@@ -1,13 +1,46 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<sstream>
 using namespace std;
-vector<string> rec;
+string rec[202];
 
-string f(int i){
-
+string Open = "sin(";
+string Opens = "(";
+string Close = ")";
+string get_n(int n) {
+	string ch = "";
+	vector<char> tmp;
+	while (n) {
+		char a = '0' + n % 10;
+		tmp.push_back(a);
+		n /= 10;
+	}
+	for (int i = tmp.size() - 1;i >= 0;i--) {
+		ch.push_back(tmp[i]);
+	}
+	return ch;
 }
 
-int main(){
+string f(int i,int limit) {
+	if (i == limit) {
+		return Open + get_n(i) + Close;
+	}
+	return Open + get_n(i)+"+"+ f(i+1,limit)+ Close;
+}
 
+string g(int i,int limit) {
+	if (i == 1) {
+		return rec[i] + "+" + get_n(limit);
+	}
+	return Opens + g(i - 1, limit) + Close + rec[i] + "+"+get_n(limit-i+1);
+}
+
+int main() {
+	int n ;cin>>n;
+	for (int i = 1;i <= n;i++) {
+		rec[i] = f(1, i);
+	}
+
+	cout << g(n, n);
 }
